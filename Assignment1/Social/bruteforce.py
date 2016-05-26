6c551b9bb6dd5a976fb9366ab9107627646e5e35
Ysee MONNIER <yseemonnier@gmail.com>
2016-05-26 21:31:18 +0200

    change struct

--- Assignment1/Social/bruteforce.py
+++ Assignment1/Social/bruteforce.py
@@ -1,17 +1,37 @@
 #
 # bruteforce.py
 # Author Ysee Monnier
+# String matching
 #
 
 import reader
 import string
 import time
 
+def bruteForce(T, P):
+	# Text length
+	n = len(T)
+
+	# Pattern length
+	m = len(P)
+
+	# Number of occurrences found
+	res = []
+
+	for i in xrange(0, n - m):
+		j = 0
+		while j < m and s[i + j] == pattern[j]:
+			j = j + 1
+		if j == m:
+			res.append(i)
+	return res
+
+
 #fileName = input('Your txt file: ')
 #pattern = input('Your pattern: ')
 
 fileName = 'book.txt'
-pattern = 'from the window or the door'
+pattern = 'of'
 
 # Get content's file
 s = reader.readFile(fileName)
@@ -19,21 +39,9 @@ s = reader.readFile(fileName)
 # Start timer
 start_time = time.time()
 
-n = len(s)
-m = len(pattern)
+#Bruteforce
+shifts = bruteForce(s, pattern)
 
-# Number of occurrences found
-res = 0
-
-# Brute force algorithm
-for i in xrange(0, n - m):
-	j = 0
-	while j < m and s[i + j] == pattern[j]:
-		j = j + 1
-	if j == m:
-		res = res + 1
-
-print("--- %s seconds ---" % (time.time() - start_time))
-print res
-
-
+print("Execution time: %s seconds" % (time.time() - start_time))
+print("%s matchings" % len(shifts))
+print("shifts " + str(shifts))
